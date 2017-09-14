e43e2b0b9f57f7cf9891cb00d8d7a9b2833f3bec
pterranova <pterranova@ippon.fr>
2017-09-14 08:37:56 +0000

    modif 2

--- HelloIppon.java
+++ HelloIppon.java
@@ -2,7 +2,7 @@ public class HelloIppon
 {
 	public static void main (String [] args)
 	{
-		System.out.println("TEST TEST TEST");
+		System.out.println("TEST TEST TEST TEST");
 		System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ");
 		System.out.println(" |_ _|  _ \\|  _ \\ / _ \\| \\ | |  _   / _(_)_ __ ___| |_    ___ ___  _ __ | |_ __ _(_)_ __   ___ _ __  | | ");
 		System.out.println("  | || |_) | |_) | | | |  \\| | (_) | |_| |  __/ __| __|  / __/ _ \\|  _ \\| __/ _` | |  _ \\ / _ \\  __| | | ");
